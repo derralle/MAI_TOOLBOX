1b864dc78399d08223bc7a1f6c78359dbfe94192
Ralph Müller <ralph@kekshirn.de>
2013-06-30 23:15:38 +0200

    Schutzzaun geändert
    Transformation im Schutzzaununterprogramm funktioniert nun

--- MAI_TOOLBOX/MAI-Toolbox/MAITOOLS.vb
+++ MAI_TOOLBOX/MAI-Toolbox/MAITOOLS.vb
@@ -955,27 +955,7 @@ Public Class MAITOOLS
 
     End Sub
 
-    Public Function GetModelKoordinaten(ByVal Punkt As Object, ByVal Transform As MathTransform, ByVal swapp As SldWorks) As MathPoint
 
-        Dim swMathUtil As MathUtility
-
-        swMathUtil = swapp.GetMathUtility
-
-        Dim swMathPoint As MathPoint
-
-        swMathPoint = swMathUtil.CreatePoint(Punkt)
-
-
-
-        Transform.Inverse()
-
-        swMathPoint.MultiplyTransform(Transform)
-
-        Return swMathPoint
-
-
-
-    End Function
 
     Public Sub Schutzzaun(ByVal Modeldoc As ModelDoc2, ByVal swapp As SldWorks)
 
@@ -989,7 +969,9 @@ Public Class MAITOOLS
         Dim komponente As Component2
         Dim transform As MathTransform
         Dim ist3DSkizze As Boolean = False
+        Dim swMathUtil As MathUtility
 
+        swMathUtil = swapp.GetMathUtility
 
         'Prüfen ob das Dokument eine Baugruppe ist
         If Modeldoc.GetType <> swDocumentTypes_e.swDocASSEMBLY Then
@@ -1020,44 +1002,64 @@ Public Class MAITOOLS
         'Prüfen ob Skizze eine 3D-Skizze ist. Eine
         '2D-Skizze 
         If sketch.Is3D = False Then
+            'Transform Matrix erstellen 
             transform = sketch.ModelToSketchTransform
+
+            'Umkehrmatrix erzeugen
+            transform = transform.Inverse
+
+
         Else
 
             transform = Nothing
 
         End If
 
-
+        'Linienarray durchlaufen
         For Each item As SketchLine In lines
-            Dim punkt As New MathPoint
 
-            punkt.ArrayData = item.GetStartPoint2
+            Dim mpunkt As MathPoint
+            Dim spunkt As SketchPoint
+            Dim coords(2) As Double
+
+
+            'Startkoordinaten Skizzenpunkt 
+            spunkt = item.GetStartPoint2
+
+
+
+            coords(0) = spunkt.X
+            coords(1) = spunkt.Y
+            coords(2) = spunkt.Z
+
+            mpunkt = swMathUtil.CreatePoint(coords)
+
+
+
+            'punkt.ArrayData = item.GetStartPoint2
 
             If sketch.Is3D = False Then
 
-                punkt = GetModelKoordinaten(punkt, transform, swapp)
-
+                mpunkt = mpunkt.MultiplyTransform(transform)
 
             End If
 
 
+            points.Add(mpunkt)
 
-
-            'MsgBox("X: " & punkt.X & vbNewLine & _
-            '     "Y: " & punkt.Y & vbNewLine & _
-            '      "Z: " & punkt.Z)
-
-            points.Add(punkt)
         Next
 
 
 
-        For Each item In points
 
-            MsgBox("X: " & item.X & vbNewLine & _
-                   "Y: " & item.Y & vbNewLine & _
-                   "Z: " & item.Z)
-            komponente = SwAssy.AddComponent5("DP-F-2200 Durchgangspfosten.SLDPRT", False, "", False, "", item.X, item.Y, item.Z)
+
+
+        For Each item As MathPoint In points
+
+            '' MsgBox("X: " & item.X & vbNewLine & _
+            ''        "Y: " & item.Y & vbNewLine & _
+            ''        "Z: " & item.Z)
+            komponente = SwAssy.AddComponent5("DP-F-2200 Durchgangspfosten.SLDPRT", False, "", False, "", item.ArrayData(0), item.ArrayData(1), item.ArrayData(2))
 
         Next
 
@@ -1177,6 +1179,7 @@ Public Class MAITOOLS
 
 
     'Fremdcode!!!
+
     ''' <summary>
     ''' Ermittelt die aktive Skizze (in Bearbeitung)
     ''' eines SolidWorks Dokuments und gibt
