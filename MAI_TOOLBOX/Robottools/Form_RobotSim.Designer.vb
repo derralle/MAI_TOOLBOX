a9dc4ed16f73041a6787582636b10c50e000e235
Ralph Müller <ralph@kekshirn.de>
2014-04-23 08:03:09 +0200

    start

---
+++ MAI_TOOLBOX/Robottools/Form_RobotSim.Designer.vb
@@ -0,0 +1,37 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form_RobotSim
+    Inherits System.Windows.Forms.Form
+
+    'Das Formular überschreibt den Löschvorgang, um die Komponentenliste zu bereinigen.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Wird vom Windows Form-Designer benötigt.
+    Private components As System.ComponentModel.IContainer
+
+    'Hinweis: Die folgende Prozedur ist für den Windows Form-Designer erforderlich.
+    'Das Bearbeiten ist mit dem Windows Form-Designer möglich.  
+    'Das Bearbeiten mit dem Code-Editor ist nicht möglich.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.SuspendLayout()
+        '
+        'Form_RobotSim
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(465, 517)
+        Me.Name = "Form_RobotSim"
+        Me.Text = "Form_RobotSim"
+        Me.ResumeLayout(False)
+
+    End Sub
+End Class
---
+++ MAI_TOOLBOX/Robottools/RobotSim.vb
@@ -0,0 +1,28 @@
+﻿Imports SolidWorks.Interop.sldworks
+Imports SolidWorks.Interop.swconst
+Imports System.Collections.Generic
+
+
+Public Class RobotSim
+
+    'SolidWorks-App
+    Private _SwApp As ISldWorks
+    Public Property SwApp() As ISldWorks
+        Get
+            Return _SwApp
+        End Get
+        Set(ByVal value As ISldWorks)
+            _SwApp = value
+        End Set
+    End Property
+
+
+    Sub New(ByRef swinst As ISldWorks)
+        Me.SwApp = swinst
+    End Sub
+
+
+
+
+
+End Class
